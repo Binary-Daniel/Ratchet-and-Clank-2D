e287c726319464a4a99c182e3861dad151331c8b
Binary-Daniel <daniel.esteen.edu@rattvik.se>
2022-04-27 14:45:08 +0200

    Demo

---
+++ bin/Debug/dep/res/Collision.cpp.d
@@ -0,0 +1,6 @@
+bin/Debug/obj/res/Collision.cpp.o: src/res/Collision.cpp \
+ C:/SFML-2.5.1/include/SFML/Graphics.hpp src/res/Collision.hpp
+
+C:/SFML-2.5.1/include/SFML/Graphics.hpp:
+
+src/res/Collision.hpp:
